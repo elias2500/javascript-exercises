2add6a91c6c60736ff70e2c4c7c740fc0c3b2b9f
Ilias Kaleridis <eliaskaler@gmail.com>
2025-09-26 11:15:25 +0300

    Revers string
    Using the spread operator to turn the string into
    an array, .reverse() to reverse the array, and
    join to turn the array back to string. The
    .toString method is not suitable here because it
    returns each character separated by comma.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArr = [...string];
+    stringArr.reverse();
+    return stringArr.join('');
 };
 
 // Do not edit below this line
