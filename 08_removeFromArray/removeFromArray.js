011bf3bd8cad2d2104fc5fb47ccac1a2dfaf9dda
Ilias Kaleridis <eliaskaler@gmail.com>
2025-09-26 11:40:48 +0300

    Add function that removes givet items from array
    Function that gets an array and an unknown number
    of items to be removed and removes them from the
    array.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toBeRemovedItems) {
+    for (let i=0; i<toBeRemovedItems.length; i++){
+        for (let j=0; j<array.length; j++){
+            if (array[j] === toBeRemovedItems[i]){
+                array.splice(j, 1);
+                j = 0;
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
