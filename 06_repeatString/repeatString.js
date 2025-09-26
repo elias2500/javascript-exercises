22e2c7850e5891cae8cb6de4c9af9a383104de37
Ilias Kaleridis <eliaskaler@gmail.com>
2025-09-26 11:07:25 +0300

    Write function that repeats given string using loops

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatNumber) {
+    if (repeatNumber < 0){
+        return 'ERROR';
+    }
+    
+    let resultString='';
+    for(let i=0; i<repeatNumber; i++){
+        resultString += string;
+    }
+    return resultString;
 };
 
 // Do not edit below this line
